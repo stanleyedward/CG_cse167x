1132bdb8ac007e6bd40c5f9605ed6f58986f76f3
stanleyedward <stanleyedward9715@gmail.com>
2024-02-24 18:41:29 +0530

    complete rotation

--- hw1-linux_osx/Transform.cpp
+++ hw1-linux_osx/Transform.cpp
@@ -8,9 +8,26 @@
 // Helper rotation function.  
 mat3 Transform::rotate(const float degrees, const vec3& axis) {
 	// YOUR CODE FOR HW1 HERE
+	mat3 Identity = glm::mat3(1.0);
 
+	mat3 Axis_Axis_T = glm::mat3(
+		axis.x*axis.x, axis.x*axis.y, axis.y*axis.z,
+	  	axis.x*axis.y, axis.y*axis.y, axis.y*axis.z,
+	  	axis.x*axis.z, axis.y*axis.z, axis.z*axis.z
+	);  Axis_Axis_T= glm::transpose(Axis_Axis_T);
+
+	mat3 A_star = glm::mat3(
+	  0, axis.z, -axis.y,
+	  -axis.z, 0, axis.x,
+	  axis.y, -axis.x, 0);
+
+	float radians = degrees * (pi/180);
+	float sin_angle = sin(radians);
+	float cos_angle = cos(radians);
+	
+	mat3 Rotation = glm::mat3( cos_angle*Identity + (1-cos_angle)*Axis_Axis_T + sin_angle*A_star);
 	// You will change this return call
-	return mat3();
+	return Rotation;
 }
 
 // Transforms the camera left around the "crystal ball" interface
