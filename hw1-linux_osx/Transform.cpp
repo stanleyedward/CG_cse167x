803afdd9ed0cd4c90fa6e372539f6809abc173bd
stanleyedward <stanleyedward9715@gmail.com>
2024-02-25 13:56:03 +0530

    complete transform for left and up

--- hw1-linux_osx/Transform.cpp
+++ hw1-linux_osx/Transform.cpp
@@ -2,7 +2,8 @@
 
 
 #include "Transform.h"
-
+#include <stdio.h>
+#include <iostream>
 //Please implement the following functions:
 
 // Helper rotation function.  
@@ -33,11 +34,20 @@ mat3 Transform::rotate(const float degrees, const vec3& axis) {
 // Transforms the camera left around the "crystal ball" interface
 void Transform::left(float degrees, vec3& eye, vec3& up) {
 	// YOUR CODE FOR HW1 HERE
+	mat3 rotation = rotate(degrees, vec3(0.0, 1.0, 0.0));
+	eye = eye * rotation;
+	up = up * rotation;
+	printf("Coordinates: %.2f, %.2f, %.2f; distance: %.2f\n", eye.x, eye.y, eye.z, sqrt(pow(eye.x, 2) + pow(eye.y, 2) + pow(eye.z, 2)));
 }
 
 // Transforms the camera up around the "crystal ball" interface
 void Transform::up(float degrees, vec3& eye, vec3& up) {
 	// YOUR CODE FOR HW1 HERE 
+	mat3 rotation = rotate(degrees, vec3(1.0, 0.0, 0.0));
+	eye = eye * rotation;
+	up = up * rotation;
+	printf("Coordinates: %.2f, %.2f, %.2f; distance: %.2f\n", eye.x, eye.y, eye.z, sqrt(pow(eye.x, 2) + pow(eye.y, 2) + pow(eye.z, 2)));
+
 }
 
 // Your implementation of the glm::lookAt matrix
