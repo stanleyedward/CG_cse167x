8b14548480c5d953858c6f80417d5e0299da4577
stanleyedward <stanleyedward9715@gmail.com>
2024-03-16 19:23:00 +0530

    frag change

--- hw2-windows/hw2-windows/shaders/light.frag.glsl
+++ hw2-windows/hw2-windows/shaders/light.frag.glsl
@@ -34,18 +34,49 @@ uniform vec4 specular;
 uniform vec4 emission; 
 uniform float shininess; 
 
+vec4 ComputeLight (const in vec3 direction, const in vec4 lightcolor, const in vec3 normal, const in vec3 halfvec, const in vec4 mydiffuse, const in vec4 myspecular, const in float myshininess) {
+		float nDotL = dot(normal, direction);         
+        vec4 lambert = mydiffuse * lightcolor * max (nDotL, 0.0) ;  
+
+        float nDotH = dot(normal, halfvec) ; 
+        vec4 phong = myspecular * lightcolor * pow (max(nDotH, 0.0), myshininess) ; 
+
+        vec4 retval = lambert + phong ; 
+        return retval;            
+} 
+
 void main (void) 
 {       
     if (enablelighting) {       
-        vec4 finalcolor; 
+        vec4 finalcolor = vec4(0.0f); 
+
+		const vec3 eyepos = vec3(0,0,0) ; 
+        vec3 mypos = myvertex.xyz / myvertex.w ; // Dehomogenize current location 
+        vec3 eyedirn = normalize(eyepos - mypos) ; 
+
+        // Compute normal, needed for shading.
+		vec3 normal = normalize(mynormal);
 
         // YOUR CODE FOR HW 2 HERE
         // A key part is implementation of the fragment shader
 
-        // Color all pixels black for now, remove this in your implementation!
-        finalcolor = vec4(0.0f, 0.0f, 0.0f, 1.0f); 
+		for(int i = 0; i < numLights; i++) {
+			vec4 color;
+			if(abs(lightposn[i].w)==0.0) {
+				//directional
+				vec3 lightdiren = normalize(lightposn[i].xyz);
+				vec3 lighthalfangle = normalize(lightdiren + eyedirn);
+				color = ComputeLight(lightdiren, lightcolor[i], normal, lighthalfangle, diffuse, specular, shininess);
+			} else {
+				//point
+				vec3 position = lightposn[i].xyz / lightposn[i].w;
+				vec3 direction = normalize(position - mypos);
+				vec3 lighthalf = normalize(direction + eyedirn);
+				color = ComputeLight(direction, lightcolor[i], normal, lighthalf, diffuse, specular, shininess);
+			} finalcolor += color;
+		}
 
-        fragColor = finalcolor; 
+        fragColor = ambient + emission + finalcolor;
     } else {
         fragColor = vec4(color, 1.0f); 
     }
